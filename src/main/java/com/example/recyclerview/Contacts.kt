25d4b08bb9e9b56666ec7f9c1851116d51d6f17d
Anyijukire Janet <79198971+Anyijukire@users.noreply.github.com>
2021-09-14 13:03:12 +0300

    Update Contacts.kt

--- src/main/java/com/example/recyclerview/Contacts.kt
+++ src/main/java/com/example/recyclerview/Contacts.kt
@@ -1,8 +1,15 @@
-package com.example.recyclerview
+package com.example.recyclerview.models
 
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts")
 data class Contacts(
+    @PrimaryKey(autoGenerate = true) @NonNull var contactId :Int,
     var name: String,
     var email: String,
     var phoneNumber: String,
-    var imageUrl:String
+    var imageUrl:String?
 )
+
